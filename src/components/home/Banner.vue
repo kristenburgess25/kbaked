921dc4c75d6f35d09480adfc5074ac69fba818f9
Kristen Burgess <kristenburgess25@Kristens-iMac.local>
2021-01-06 16:37:46 -0700

    Update banner image and fonts, add recipe assets folder and banner photo

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,7 +1,8 @@
 <template>
   <base-card dark>
     <v-img
-      :src="require('@/assets/articles/hillscows.png')"
+      :src="require('@/assets/recipes/cookies-banner.jpeg')"
+      id="banner-image"
       class="grey lighten-2"
       height="400"
       width="100%"
@@ -15,12 +16,12 @@
           md="7"
           offset-md="5"
         >
-          <h1 class="display-3 font-weight-light">
-            Kristen Burgess
+          <h1 class="font-weight-light">
+           Kristen Loves Cookies
           </h1>
 
-          <div class="subheading text-uppercase pl-2 mb-4">
-            Software Engineer 
+          <div id="banner-subhead" class="subheading text-uppercase pl-2 mb-4">
+            A baking blog by Kristen Burgess.
           </div>
 
           <!-- <v-btn
@@ -34,6 +35,28 @@
     </v-img>
   </base-card>
 </template>
+
+<style lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Yellowtail&display=swap');
+
+/* image {
+  opacity: 0.5;
+} */
+
+#banner-subhead {
+  color: purple;
+  background-color: white;
+}
+
+h1 {
+  font-family: 'Yellowtail', cursive;
+  color: teal;
+  background-color: white;
+  font-size: 3.75rem !important;
+  line-height: 3.75rem;
+  letter-spacing: -0.0083333333em !important;
+}
+</style>
 
 <script>
   export default {
