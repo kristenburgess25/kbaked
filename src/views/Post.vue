240655bc10c62bbd4dc139b384f222b721d0edee
Kristen Burgess <kristen.burgess@doane.edu>
2021-01-22 21:19:08 -0700

    Display Ingredients and Instructions on recipes

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -5,15 +5,35 @@
   >
     <h2>{{recipe.title}} </h2>
     <br />
-        <v-img
+    <v-img
         :src="require(`../assets/articles/${recipe.hero}`)"
         height="100%"
-        gradient="rgba(0, 0, 0, .42), rgba(0, 0, 0, .42)"
+        gradient="rgba(0, 0, 0, .22), rgba(0, 0, 0, .22)"
       >
-      </v-img>
+    </v-img>
+    <br />
     <p>
       {{recipe.introduction}}
     </p>
+    <v-col id="recipe-container" cols="12">
+      <div id="ingredient-container">
+        <h3> Ingredients </h3>
+        <ul id="ingredient-list">
+          <li v-for="ingredient in recipe.recipe.ingredients" :key="ingredient.index">
+            {{ ingredient }}
+          </li>
+        </ul>  
+      </div>
+      <br />
+      <div id="instructions-container">
+        <h3> Instructions </h3>
+        <ul id="instructions-list">
+          <li v-for="item in recipe.recipe.instructions" :key="item.index">
+            {{ item }}
+          </li>
+        </ul> 
+      </div>
+    </v-col>
   </v-container>
 </template>
 
@@ -34,3 +54,19 @@
     }
   }
 </script>
+
+<style>
+#instructions-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+/* #recipe-container {
+  display: flex;
+  flex-direction: row;
+} */
+
+/* #ingredient-container{
+  width: 30%;
+} */
+</style>
