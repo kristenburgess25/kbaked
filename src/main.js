760fb3cbe7965a85777b92e14858aa0e37b1d784
Kristen Burgess <kristen.burgess@doane.edu>
2021-02-25 21:49:37 -0700

    Update firebae config in main.js

--- src/main.js
+++ src/main.js
@@ -10,17 +10,19 @@ Vue.config.productionTip = false
 
 var config = {
   apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
+  authDomain: "kristenlovescookies.firebaseapp.com",
   // For databases not in the us-central1 location, databaseURL will be of the
   // form https://[databaseName].[region].firebasedatabase.app.
   // For example, https://your-database-123.europe-west1.firebasedatabase.app
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
+  databaseURL: "https://kristenlovescookies-default-rtdb.firebaseio.com/",
+  // storageBucket: "bucket.appspot.com"
 };
 firebase.initializeApp(config);
 
 // Get a reference to the database service
 var database = firebase.database();
+
+console.log('DATABAE', database)
 
 new Vue({
   vuetify,
